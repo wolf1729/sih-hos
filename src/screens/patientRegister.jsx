cdb219ec190294b1a6190b7b64868af6c6eff8ae
roastedskywalker <aditya.khazanchi@gmail.com>
2023-09-24 00:32:18 +0530

    register

---
+++ src/screens/patientRegister.jsx
@@ -0,0 +1,13 @@
+import RegisterPatient from "../components/registerP";
+import Logregback from "../components/logregback";
+
+function PatientRegister(){
+    return(
+        <>
+        <Logregback/>
+        <RegisterPatient/>
+        </>
+    );
+}
+
+export default PatientRegister;
